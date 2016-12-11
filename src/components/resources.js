c9ddb73d20076e9244fbf5822ca76abe0d884b32
Richard James <richard.james@mygravity.co>
2016-12-11 19:32:07 +0000

    First commit

---
+++ src/components/resources.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export default () => {
+
+    return(
+    
+        <div>
+            Super Special Recipe
+        
+            <ul>
+                <li>1 spoon sugar</li>
+                <li>1 spoon coffee</li>
+                <li>1 cup of water</li>
+        
+            </ul>
+        </div>
+    
+    );
+}
